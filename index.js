d83e34c6f9b4ba1f4b17a0c071ca2e0d5bf8641c
Rewat Singharatho <61160228@go.buu.ac.th>
2021-03-04 19:24:06 +0700

    create POST /books to store book data into array

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+let books = []
+
+app.post('/books', (req, res) => {
+
+    let newTitle = req.body.title
+    let newPrice = req.body.price
+    let newUnit = req.body.unit
+    let newIsbn = req.body.isbn
+    let newImage = req.body.image_url
+
+    let newBook = {
+        title: newTitle,
+        price: newPrice,
+        unit: newUnit,
+        isbn: newIsbn,
+        image_url: newImage,
+    }
+
+    let bookID = 0
+
+
+    books.push(newBook)
+
+
+    bookID = books.length - 1
+
+    res.status(201).json(bookID)
+})
+
+const port = 3000
+app.listen(3000, () => console.log(`Server started at ${port}`))
